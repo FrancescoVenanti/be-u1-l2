b60c8540050a2243b563e3d174275498212831fa
FrancescoVenanti <francesco.venanti@gmail.com>
2024-01-30 16:50:39 +0100

    Aggiungere i file di progetto.

---
+++ Persona/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    internal class Persona
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+        public string GetCognome()
+        {
+            return Surname;
+        }
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public string GetDetails()
+        {
+            return $"nome: {Name} Cognome: {Surname} eta': {Age}";
+        }
+        public Persona(string nome, string cognome, int age) { 
+            this.Name = nome;
+            this.Surname = cognome;
+            this.Age = age;
+        }
+    }
+}
---
+++ Persona/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("mario", "rossi", 34);
+
+            Console.WriteLine(p1.GetDetails());
+            
+            Console.ReadLine();
+        }
+    }
+}
